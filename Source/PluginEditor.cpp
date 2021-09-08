73e582acd2382e015614c04250c7ad14c92a24b5
soura-vvv <souravpande39@gmail.com>
2021-09-08 15:14:50 +0530

    Added Frequency labels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -267,7 +267,7 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
         responseCurve.lineTo(responseArea.getX() + i, map(mags[i]));
     }
 
-    g.setColour(Colours::orangered);
+    g.setColour(Colours::lightyellow);
     g.drawRoundedRectangle(getRenderArea().toFloat(), 4.f, 1.f);
 
     g.setColour(Colours::white);
@@ -275,9 +275,9 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 
     Array<float> freqs
     {
-        20,30,40,50,100,
-        200,300,400,500,1000,
-        2000,3000,4000,5000,10000,
+        20,50,100,
+        200,500,1000,
+        2000,5000,10000,
         20000
     };
     auto renderArea = getAnalysisArea();
@@ -313,6 +313,36 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
     }
     
     g.drawRect(getAnalysisArea());
+
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+    for (int i = 0; i < freqs.size(); ++i)
+    {
+        auto f = freqs[i];
+        auto x = xs[i];
+
+        bool addK = false;
+        String str;
+        if (f > 999.f)
+        {
+            addK = true;
+            f /= 1000.f;
+
+        }
+        str << f;
+        if (addK)
+            str << "k";
+        str << "Hz";
+
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+
+    }
 }
 
 void ResponseCurveComponent::resized()
